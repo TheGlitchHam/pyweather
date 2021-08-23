f48c50d70047781bc81bc6dddb6ff0ac29d08f85
TheGlitchHam <36936985+TheGlitchHam@users.noreply.github.com>
2021-08-23 21:08:51 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,5 +3,5 @@ import sys, time, requests
 while True:
     resp = requests.get(f'https://wttr.in/{sys.argv[1].replace(" ", "+")}')
     print(resp.text)
-    time.sleep(600) #in secs
+    time.sleep(3600) #in secs
     print("Restarting")
